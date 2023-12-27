96c0ea5a07f7ed2f356de23f9f3b83df4fccc439
myeonginjin <113578420+myeonginjin@users.noreply.github.com>
2023-12-28 03:27:43 +0900

    Initial Commit

---
+++ PICSPLAY3/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  PICSPLAY3
+//
+//  Created by 진명인 on 12/28/23.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
