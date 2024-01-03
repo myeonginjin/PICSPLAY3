c3c9903820154dd04064544b0a7ec3c1790cbb7e
jaein <jaein@jellybus.com>
2024-01-03 19:35:07 +0900

    [PICSPLAY-6] Core Image 프레임워크 사용 해제, Core Grapics 사용

--- PICSPLAY3/EditViewController.swift
+++ PICSPLAY3/EditViewController.swift
@@ -7,14 +7,25 @@
 
 import UIKit
 
+struct RGBAPixel {
+    var red: UInt8
+    var green: UInt8
+    var blue: UInt8
+    var alpha: UInt8
+}
+
 class EditViewController: UIViewController {
     @IBOutlet var monochromeImgView: UIImageView!
 
     //imageToShow에는 편집된 사진 객체인 monochromeImgView가 prepare매소드를 통해 전달되어 있음
     var imageToShow: UIImage?
     
+    var appliedGrayScaleImage: UIImage?
+    
     //n회 이상 흑백 전환을 수행하기 위해 현재 이미지의 필터적용 여부 기록할 변수 지정
     var isOriginalImage = true
+    
+    var readyToShow = false
 
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -39,7 +50,10 @@ class EditViewController: UIViewController {
         
         if isOriginalImage {
             // 현재 원본 이미지인 경우 흑백 필터 적용
-            applyMonochromeFilter(to: image)
+            
+            if let monochromeImage = applyMonochromeFilter(image: image) {
+                monochromeImgView.image = monochromeImage
+            }
         } else {
             // 현재 흑백 필터가 적용된 이미지인 경우 원본 이미지로 변경
             monochromeImgView.image = imageToShow
@@ -48,35 +62,84 @@ class EditViewController: UIViewController {
         // 필터적용 여부 반전
         isOriginalImage.toggle()
     }
-        
-    //흑백 필터 적용 매소드
-    func applyMonochromeFilter(to image: UIImage) {
-        
-        //Core Image 프레임워크의 프로세싱(이미지 처리 결과 렌더링 및 이미지 분석)을 수행할 CIContext 생성
-        let context = CIContext(options: nil)
-        
-        //immutable한 UIImage 객체를 편집하기 위해 CIImage 형식으로 변환
-        guard let ciImage = CIImage(image: image) else { return }
-
-        //흑백필터를 적용하는 CIFilter 객체 CIPhotoEffectMono를 filter에 지정
-        if let filter = CIFilter(name: "CIPhotoEffectMono") {
-
-            //키값 kCIInputImageKey를 통해 ciImage를 입력이미지로 지정
-            filter.setValue(ciImage, forKey: kCIInputImageKey)
-
-            //프로세싱이 완료된 출력이미지를 outputCIImage에 지정
-            if let outputCIImage = filter.outputImage,
-               
-                //뷰어를 통해 나타내거나 파일에 저장할 수 있는 UIImage로 변환하기 위해 우선 cgImage 형식으로 렌더링
-               let cgImage = context.createCGImage(outputCIImage, from: outputCIImage.extent) {
-                
-                //UIKit에서 사용할 수 있는 형식인 UIImage로 변환
-                monochromeImgView.image = UIImage(cgImage: cgImage)
-            
-
-            }
+    // 픽셀 단위로 이미지 조작하는 함수
+    func applyMonochromeFilter(image: UIImage) -> UIImage? {
+        guard let cgImage = image.cgImage else {
+            return nil
         }
+        
+        let pixelData = cgImage.dataProvider!.data    // 이미지에서를 Data 형태로 만듭니다.
+        guard let data = CFDataGetBytePtr(pixelData) else {
+            return nil  // 또는 다른 적절한 값으로 처리
+        }  // 주소로 접근할 수 있도록 선언합니다.
+        
+        guard let grayScaleImage = getGrayScaleUIImage(pData: data, image: image) else {
+            return nil
+        }
+
+        return grayScaleImage
+
+
     }
+
+
+
+//    func getGrayScaleUIColor(pData: UnsafePointer<UInt8>, _ pixel: Int) -> UIColor {
+//        let red = pData[pixel]
+//        let green = pData[(pixel + 1)];
+//        let blue = pData[pixel + 2];
+//        let alpha = pData[pixel + 3];
+//
+////펴균값
+//        return UIColor(red: CGFloat(red) * 0.2126 /255.0 , green: CGFloat(green) * 0.7152 /255.0, blue: CGFloat(blue) * 0.0722 /255.0, alpha: CGFloat(alpha)/255.0)
+//    }
+
+
+
+    func getGrayScaleUIImage(pData: UnsafePointer<UInt8> , image : UIImage) -> UIImage? {
+        let width = Int(image.size.width)
+        let height = Int(image.size.height)
+
+        let bitsPerComponent = 8
+        let bytesPerPixel = 4
+        let bytesPerRow = bytesPerPixel * width
+        let colorspace = CGColorSpaceCreateDeviceRGB()
+        let bitmapInfo: UInt32 = CGBitmapInfo.byteOrder32Big.rawValue | CGImageAlphaInfo.premultipliedLast.rawValue
+
+        // 픽셀 데이터로 사용할 메모리를 할당
+        let rawdata = UnsafeMutablePointer<RGBAPixel>.allocate(capacity: width * height)
+        defer {
+            rawdata.deallocate()
+        }
+
+        for i in 0..<width * height {
+            let gray = UInt8((Int(pData[i * bytesPerPixel]) + Int(pData[i * bytesPerPixel + 1]) + Int(pData[i * bytesPerPixel + 2])) / 3)
+            rawdata[i] = RGBAPixel(red: gray, green: gray, blue: gray, alpha: pData[i * bytesPerPixel + 3])
+            
+        }
+
+        // CGContext를 생성하고 픽셀 데이터를 이용하여 이미지를 그립니다.
+        guard let context = CGContext(data: rawdata, width: width, height: height, bitsPerComponent: bitsPerComponent, bytesPerRow: bytesPerRow, space: colorspace, bitmapInfo: bitmapInfo, releaseCallback: nil, releaseInfo: nil) else {
+            return nil
+        }
+                // CGImage를 생성합니다.
+        guard let cgImage = context.makeImage() else {
+            return nil
+        }
+
+        // CGImage를 UIImage로 변환하여 반환합니다.
+        return UIImage(cgImage: cgImage)
+
+
+
+    }
+
+//
+//    func renderImage(colors : [UIColor]) -> UIImage? {
+//
+//
+//        return grayScaleUIImage
+//    }
 
     //현재 뷰어에 띄워져 있는 사진 객체 기기 라이브러리에 저장
     @IBAction func saveToLibrary(_ sender: UIButton) {
@@ -115,4 +178,5 @@ class EditViewController: UIViewController {
         self.present(alert, animated: true, completion: nil)
     }
 }
+
 
